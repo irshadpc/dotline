131f6ba3063e71785f441f0a4d5034915b9ad8f5
Irshad PC <irshad.pc@hatio.in>
2019-07-10 10:45:52 +0530

    Remove external dependency

--- App.js
+++ App.js
@@ -5,69 +5,73 @@
  * @format
  * @flow
  */
-import React, { Component } from 'react';
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
+import React, { Component } from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-native";
 
-import PasswordGesture from 'react-native-gesture-password';
+import PasswordGesture from "react-native-gesture-password";
 
-var Password1 = '123';
+var Password1 = "";
 
 export default class HelloWorldApp extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        message: 'Please input your password.',
-        status: 'normal'
-    }
+      message: "Please input your password.",
+      status: "normal"
+    };
   }
+
   onEnd(password) {
-    if (password == Password1) {
+    if (Password1 === "") {
+      // The first password
+      Password1 = password;
+      this.setState({
+        status: "normal",
+        message: "Please input your password secondly."
+      });
+    } else {
+      // The second password
+      if (password === Password1) {
         this.setState({
-            status: 'right',
-            message: 'Password is right, success.'
+          status: "right",
+          message: "Your password is set to " + password
         });
 
+        Password1 = "";
         // your codes to close this view
-    } else {
+      } else {
         this.setState({
-            status: 'wrong',
-            message: 'Password is wrong, try again.'
-          });
+          status: "wrong",
+          message: "Not the same, try again."
+        });
       }
+    }
   }
 
   onStart() {
-    this.setState({
-        status: 'normal',
-        message: 'Please input your password.'
-    });
-  }
-
-  onReset() {
-    this.setState({
-        status: 'normal',
-        message: 'Please input your password (again).'
-    });
+    if (Password1 === "") {
+      this.setState({
+        message: "Please input your password."
+      });
+    } else {
+      this.setState({
+        message: "Please input your password secondly."
+      });
+    }
   }
 
   render() {
     return (
-        <PasswordGesture
-            ref='pg'
-            status={this.state.status}
-            message={this.state.message}
-            onStart={() => this.onStart()}
-            onEnd={(password) => this.onEnd(password)}
-            innerCircle={true}
-            outerCircle={true}
-        />
+      <PasswordGesture
+        ref="pg"
+        status={this.state.status}
+        message={this.state.message}
+        onStart={() => this.onStart()}
+        onEnd={password => this.onEnd(password)}
+        innerCircle={true}
+        outerCircle={true}
+      />
     );
-}
-  
+  }
 }
